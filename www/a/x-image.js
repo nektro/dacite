b0b2b0954346696b08e4b2399c4e2d0b7145ee04
Meghan <hello@nektro.net>
2020-09-26 19:47:23 -0700

    www- include jpeg in list of image types

--- www/a/x-image.js
+++ www/a/x-image.js
@@ -7,7 +7,7 @@ const observer = new IntersectionObserver((changes) => {
             const parent = change.target.parentElement.getAttribute("data-src");
             const ext = parent.substring(parent.lastIndexOf(".")+1);
             const icon = `https://unpkg.com/file-icon-vectors@1.0.0/dist/icons/vivid/${ext}.svg`;
-            const images = ["png", "jpg", "gif"];
+            const images = ["png", "jpg", "jpeg", "gif"];
             const newsrc = images.includes(ext) ? parent : icon;
             change.target.setAttribute("src", newsrc);
             observer.unobserve(change.target);
